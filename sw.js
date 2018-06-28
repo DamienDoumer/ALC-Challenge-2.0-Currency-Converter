7ebeab5f014c2933d3bc0ac790a287147d793bfa
Damien <damientohin@gmail.com>
2018-06-28 07:17:12 +0100

    I ameliorated sw

--- sw.js
+++ sw.js
@@ -2,18 +2,18 @@
 
 self.addEventListener('fetch', function(event) {
 
+    let url = event.request.url;
     
-    if(event.request.url == 'https://free.currencyconverterapi.com/api/v5/currencies'){
+    if(url == 'https://free.currencyconverterapi.com/api/v5/currencies'){
         console.log("Intercepted");
     }
 
     //If the convert API is called
-    if(event.request.url.indexOf("convert") >= 0){
+    if(url.indexOf("v5/convert?") >= 0){
 
-        let url = 'https://free.currencyconverterapi.com/api/v5/convert?q=APN_JKI&compact=ultra';
         let portion1 = url.substring(url.indexOf('=')+1);
         let query = portion1.slice(0, portion1.indexOf('&'));
-        
+        console.log(query);
     }
     
 });
